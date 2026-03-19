f6326f03e5a87c70f9deb0f39b3f9aea47b92a15
Alan Santos <alansousa@outlook.com>
2026-03-19 00:07:30 -0300

    feat: add `Poppins` font-family

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "@/styles/main.scss";
+import "@fontsource/poppins";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
